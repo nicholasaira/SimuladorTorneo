5d55349b0b4f2d46fae6f0d62b2387f5876c5ed5
SpataroFranco <spatarofranco1@gmail.com>
2026-05-22 16:32:34 -0300

    Creo clase simulador y corrijo detalles

--- src/herramientas/excepciones.py
+++ src/herramientas/excepciones.py
@@ -8,32 +8,32 @@ class defensaError(Exception):
     def __init__(self,mensaje="La defensa debe estar entre 0 y 100"):
         super().__init__(mensaje)
 
-
 # Lanza error cuando la resistencia no esta entre 0 y 100
 class resistenciaError(Exception):
     def __init__(self,mensaje="La resistencia debe estar entre 0 y 100"):
         super().__init__(mensaje)
-
 
 # Lanza error cuando el ranking no esta entre 1 y 100
 class rankingError(Exception):
     def __init__(self,mensaje="El ranking debe estar entre 1 y 100"):
         super().__init__(mensaje)
 
-
 # Lanza error cuando el nivel de entrenamiento no esta entre 0 y 100
 class nivelEntrenamientoError(Exception):
     def __init__(self,mensaje="El nivel debe estar entre 0 y 100"):
         super().__init__(mensaje)
-
 
 # Lanza error cuando el estado fisico no esta entre 0 y 100
 class estadoFisicoError(Exception):
     def __init__(self,mensaje="El estado fisico debe estar entre 0 y 100"):
             super().__init__(mensaje)
 
-
 # Lanza error cuando se ingresa un equipo con el nombre vacio
 class nombreEquipoError(Exception):
     def __init__(self,mensaje="El equipo debe tener un nombre"):
+        super().__init__(mensaje)
+
+# Lanza error si la cantidad de equipos es menor a 2
+class cantidadEquiposError(Exception):
+    def __init__(self, mensaje="La cantidad de equipos debe ser mayor a 1 y par"):
         super().__init__(mensaje)
--- src/logica/simulador.py
+++ src/logica/simulador.py
@@ -1,7 +1,50 @@
 import threading
+import time
 
 class Simulador:
     def __init__(self):
-
-        self._hilos = []
+        self._threads = []
+        self._resultados = []
         self._lock = threading.Lock()
+
+    #Metodo que retorna el threads
+    def get_threads(self):
+        return self._threads
+
+    #Metodo que retorna los resultados
+    def get_resultados(self):
+        return self._resultados
+    
+    #Limpia los resultados del simulador
+    def limpiar(self):
+        self._threads.clear()
+        self._resultados.clear()
+    
+    #Metodo que realiza la simulacion del partido
+    def simular_partido(self,partido):
+        print("Simulando: ",partido)
+
+        time.sleep(2) #Comienza el partido
+        partido.jugar_partido()
+
+        with self._lock:
+            self._resultados.append(partido)
+        print("Partido finalizado: ",partido)
+
+    #Metodo para ejecutar varias simulaciones (que se jueguen varios partidos en simultaneo)
+    def ejecutar_simulaciones(self,partidos):
+        self._threads = []
+        for partido in partidos:
+            thread = threading.Thread(target=self.simular_partido,args=(partido,))
+            self._threads.append(thread)
+            thread.start()
+        for thread in self._threads:
+            thread.join()
+
+    def mostrar_resultados(self):
+        print("\n========== RESULTADOS ==========")
+        for partido in self._resultados:
+            partido.mostrar_resultado()
+
+    def __str__(self):
+        return f"Simulador con {len(self._threads)} hilos"
